5a5f2ef94ff4359d3fd5718352c82d918ae7b54a
Eduardo Camacho <57262325+edsoglez@users.noreply.github.com>
2024-12-03 14:42:35 -0600

    transparent header

--- header.js
+++ header.js
@@ -1,3 +1,11 @@
 document.getElementById('logo-cont').innerHTML = `
-    <img src="calesacoffeeshop.jpg" alt="" class="header-logo">
+    <img src="logotrans.png" alt="" class="header-logo" style="mix-blend-mode: darken;">
 `
+window.addEventListener("scroll", function () {
+    let header = document.getElementById("header-banner");
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    
+    header.style.background = `rgba(255,255,255,${1 - Math.min(scrollTop / 500,0.2)})`
+
+
+ });
