86ad367df4156a027360d02053e1de5db04f601e
Eduardo Camacho <57262325+edsoglez@users.noreply.github.com>
2025-01-13 12:35:50 -0600

    style

--- header.js
+++ header.js
@@ -1,11 +1,11 @@
 document.getElementById('logo-cont').innerHTML = `
-    <img src="logotrans.png" alt="" class="header-logo" style="mix-blend-mode: darken;">
+    <img src="calesalogosimple2.png" alt="" class="header-logo">
 `
 window.addEventListener("scroll", function () {
     let header = document.getElementById("header-banner");
     let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     
-    header.style.background = `rgba(255,255,255,${1 - Math.min(scrollTop / 500,0.4)})`
+    header.style.background = `rgba(0,0,0,${1 - Math.min(scrollTop / 500,0.4)})`
 
 
  });
